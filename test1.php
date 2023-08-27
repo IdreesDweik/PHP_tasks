7b0b3cbb3f4bf850dbc7c8a2e4e3662eca6e45c8
IdreesDweik <idrees.dweik@gmail.com>
2023-08-27 12:40:16 +0300

    update

--- test1.php
+++ test1.php
@@ -5,12 +5,10 @@ class students  {
     protected $gender;
     protected $avarage;
 
-
     public function setName($name){
-        $this->name  =$name;
+        $this -> name = $name;
         echo "this is the parent class welcome ". $this->name;
     }
-
 
     public function getName(){
         echo $this->name;
@@ -20,26 +18,26 @@ class students  {
     // $userno = new students();
     // $userno->name = "ahmad";
 
-    $obj=new students();
-    $obj->setName("idrees<br>");
+    $obj = new students();
+    $obj -> setName("baidaa<br>");
 
-class student extends students{
-public function setName($name){
-    $this->name  =$name;
-    echo "this ius the sub class welcome ".$this->name;
-}
-public function getAge($age){
-    echo $this->age;
+class user extends students{
+    public function setName($name){
+        $this->name  =$name;
+        echo "this is the sub class welcome ".$this->name;
+    }
 
-}
-public function getName(){
-    echo $this->name;
+    public function getAge($age){
+        echo $this->age;
+    }
 
+    public function getName(){
+        echo $this->name;
+    }
 }
-}
-$obj1=new student();
-$obj1->setName("hussain<br>");
-$obj1->getName();
-$obj->getName();
 
-?>
+    $obj1 = new user();
+    $obj1->setName("yazeed<br>");
+    
+    // $obj->getName();
+    // $obj1->getName();
--- test3.php
+++ test3.php
@@ -4,43 +4,43 @@
 
     <?php
 
-        //Open a file for read only. File pointer starts at the beginning of the file
-        //$myfile = fopen("php.txt", "r") or die("Unable to open file!");
+        // Open a file for read only. File pointer starts at the beginning of the file
+        // $myfile = fopen("php.txt", "r") or die("Unable to open file!");
 
-        //Open a file for write only. Erases the contents of the file or creates a new file if it doesn't exist. File pointer starts at the beginning of the file
+        // Open a file for write only. Erases the contents of the file or creates a new file if it doesn't exist. File pointer starts at the beginning of the file
         // $myfile = fopen("php.txt", "w") or die("Unable to open file!");
         // $txt = "get ready!";
         // echo fwrite($myfile, $txt);
 
-        //Open a file for write only. The existing data in file is preserved. File pointer starts at the end of the file. Creates a new file if the file doesn't exist
+        // Open a file for write only. The existing data in file is preserved. File pointer starts at the end of the file. Creates a new file if the file doesn't exist
         // $myfile = fopen("php.txt", "a") or die("Unable to open file!");
         // $txt = "r u paying attention?";
         // echo fwrite($myfile, $txt);
 
-        //Creates a new file for write only. Returns FALSE and an error if file already exists
-        //$myfile = fopen("php.txt", "x") or die("File already exist!");
+        // Creates a new file for write only. Returns FALSE and an error if file already exists
+        // $myfile = fopen("php.txt", "x") or die("File already exist!");
         // $myfile = fopen("html.txt", "x") or die("File already exist!");
         // echo fgetc($myfile);
 
-        //Open a file for read/write. File pointer starts at the beginning of the file
+        // Open a file for read/write. File pointer starts at the beginning of the file
         // $myfile = fopen("php.txt", "r+") or die("Unable to open file!");
         // $txt = "hey there";
-        //echo fwrite($myfile, $txt);
+        // echo fwrite($myfile, $txt);
 
-        //Open a file for read/write. Erases the contents of the file or creates a new file if it doesn't exist. File pointer starts at the beginning of the file
+        // Open a file for read/write. Erases the contents of the file or creates a new file if it doesn't exist. File pointer starts at the beginning of the file
         // $myfile = fopen("php.txt", "w+") or die("Unable to open file!");
         // $txt = "whats up";
         // echo fwrite($myfile, $txt);
         // echo fgetc($myfile);
 
-        //Open a file for read/write. The existing data in file is preserved. File pointer starts at the end of the file. Creates a new file if the file doesn't exist
+        // Open a file for read/write. The existing data in file is preserved. File pointer starts at the end of the file. Creates a new file if the file doesn't exist
         // $myfile = fopen("php.txt", "a+") or die("Unable to open file!");
         // $txt = "r u having fun?";
         // echo fwrite($myfile, $txt);
         // echo fgetc($myfile);
 
-        //Creates a new file for read/write. Returns FALSE and an error if file already exists
-        //$myfile = fopen("php.txt", "x+") or die("Unable to open file!");
+        // Creates a new file for read/write. Returns FALSE and an error if file already exists
+        // $myfile = fopen("php.txt", "x+") or die("Unable to open file!");
         // $myfile = fopen("html.txt", "x") or die("File already exist!");
         // $txt = "it's almost the lunch break!";
         // echo fwrite($myfile, $txt);
